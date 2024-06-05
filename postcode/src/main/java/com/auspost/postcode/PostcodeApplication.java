8344970e9aa309f38856a6b20b920311a0736e29
staceyjf <staceyfanner@gmail.com>
2024-06-06 08:22:22 +1000

    Log4j2 add

--- postcode/src/main/java/com/auspost/postcode/PostcodeApplication.java
+++ postcode/src/main/java/com/auspost/postcode/PostcodeApplication.java
@@ -2,12 +2,18 @@ package com.auspost.postcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class PostcodeApplication {
 
+	private static final Logger logger = LogManager.getLogger(PostcodeApplication.class);
+
 	public static void main(String[] args) {
+		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
 		SpringApplication.run(PostcodeApplication.class, args);
+		logger.info("PostCheck API has successfully loaded");
 	}
 
 }
